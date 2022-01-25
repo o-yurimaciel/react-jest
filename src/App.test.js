b2e2282b422bfcf9ec52eba7ecda23528a1b45eb
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2022-01-25 14:05:37 -0300

    init

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('button has correct initial color', () => {
+  render(<App />)
+  const button = screen.getByRole('button', { name: 'Change to blue' })
+  expect(button).toHaveStyle({ backgroundColor: 'red' })
+})
+
+test('button turns blue when clicked', () => {
+  render(<App />)
+  const button = screen.getByRole('button', { name: 'Change to blue' })
+  fireEvent.click(button)
+  expect(button).toHaveStyle({ backgroundColor: 'blue' })
+  expect(button.textContent).toBe('Change to red')
+})
+
+test('initial conditions', () => {
+  render(<App />)
+  const button = screen.getByRole('button', { name: 'Change to blue' })
+  expect(button).toBeEnabled()
+
+  const checkbox = screen.getByRole('checkbox')
+  expect(checkbox).not.toBeChecked()
+})
+
+test('Checkbox disables button on first click and enables on second click', () => {
+  render(<App />)
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' })
+  const button = screen.getByRole('button')
+
+  fireEvent.click(checkbox)
+  expect(button).toBeDisabled()
+
+  fireEvent.click(checkbox)
+  expect(button).toBeEnabled()
+})
